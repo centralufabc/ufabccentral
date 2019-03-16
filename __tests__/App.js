7ad42923d743033862739754ee77f3a26469770b
kleverson <nascimentokleverson@gmail.com>
2019-03-15 22:48:23 -0300

    update lint

--- __tests__/App.js
+++ __tests__/App.js
@@ -3,12 +3,11 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
